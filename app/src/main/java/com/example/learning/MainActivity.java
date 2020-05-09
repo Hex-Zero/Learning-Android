53b61681747543d7067713f7cff5b11271d41641
Ace Alrich <hex0@live.com>
2020-05-09 18:57:20 +0100

    working webview

--- app/src/main/java/com/example/learning/MainActivity.java
+++ app/src/main/java/com/example/learning/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.learning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webview = findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("file:///android_asset/main.html");
     }
 }
