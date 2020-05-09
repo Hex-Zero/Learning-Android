5db1202d73438dad76d820a49202907003b7cf5a
Ace Alrich <hex0@live.com>
2020-05-09 19:07:11 +0100

    Go back

--- app/src/main/java/com/example/learning/MainActivity.java
+++ app/src/main/java/com/example/learning/MainActivity.java
@@ -3,17 +3,34 @@ package com.example.learning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-
+   public WebView webview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView webview = findViewById(R.id.webview);
+        webview = findViewById(R.id.webview);
         webview.getSettings().setJavaScriptEnabled(true);
+
+        webview.setWebChromeClient(new WebChromeClient());
+        webview.setWebViewClient(new WebViewClient());
+
         webview.loadUrl("file:///android_asset/main.html");
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if(webview.canGoBack()){
+            webview.goBack();
+        }else {
+            finish();
+        }
     }
 }
